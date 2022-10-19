fdf2d9d61f10899c3404b92ca28a47669846cc24
F180243 <f180243@nu.edu.pk>
2022-10-19 11:32:19 +0500

    Basic types in Kotlin

--- KotlinLang/src/main/java/com/example/kotlinlang/MyClass.kt
+++ KotlinLang/src/main/java/com/example/kotlinlang/MyClass.kt
@@ -1,4 +1,40 @@
 package com.example.kotlinlang
 
-class MyClass {
+fun main()
+{
+    // var mutable
+    // val immutable
+
+
+    val  num0: Byte = 123 // Byte 8 bits
+
+    println(num0)
+
+    var  num1: Short = 123 // Short 16 bits
+    num1 = 23676
+    println(num1)
+
+    var  num2: Int = 123 // Int 32 bits
+    num2 = 23676754
+    println(num2)
+
+    var  num3: Long = 123 // Long 64 bits
+    num3 = 2367675409807855346
+    println(num3)
+
+
+    val num4: Float = 2.231213424423423f
+    println(num4)
+
+    val num5 = 2.2312134345324
+    println(num5)
+
+    val str: String = "Hello"
+    println(str)
+
+
+
+
+
+
 }
